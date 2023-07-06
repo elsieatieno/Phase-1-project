0b5bb9079d99700507d5ff4f556987a80324a45a
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-07-06 14:58:17 +0300

    Used DOM manipulation

--- index.js
+++ index.js
@@ -7,33 +7,133 @@ form.addEventListener('submit',function (event){
     event.preventDefault()
     let form_input = event.target.search_id.value;
     searchCharacter(form_input)
+    form.reset()
 })
 
-function searchCharacter(){
+function allCharacters(){
     fetch(`http://localhost:3000/superheroes`)
     .then(res => res.json())
-    .then(data => displayCharacter(data))
+    .then(data => console.log(data))
 }
 
-function displayCharacter(data, character_id){
-    const character = data.find(character => {character_id === character.id});
-    if (character) {
+function searchCharacter(search_term){
+    fetch(`http://localhost:3000/superheroes?name=${search_term}`)
+    .then(res => res.json())
+    .then(data => {
+        data.length > 0 ?  displayCharacter(data[0]): "Data not found"
+    })
+}
+
+function displayCharacter(character){
         const display = document.querySelector('#image_display');
         display.innerHTML = ' ';
 
-        const name = document.querySelector('.character_name')
+        const image = document.createElement('img')
+        image.classList = 'character'
+        image.src = character['images']['lg'];
+        image.alt = character.name
+
+        const name = document.createElement('p')
+        name.classList = 'character_name'
         name.textContent = character.name
 
-        const image = document.querySelector('.character')
-        image.src = character.image;
-        image.alt = character.name
 
         display.appendChild(name)
         display.appendChild(image)
-        name.addEventListener('click', showInfo)
-        }}
+        name.addEventListener('click', function(){
+            showInfo(character)})
 
-function showInfo(){
+    }
+
+function showInfo(character){
+    // const information = document.querySelector('#character_info')
+
+    const imageInfo = document.querySelector('#image')
+    imageInfo.src = character.images.md
+
+    const nameInfo = document.querySelector('#name')
+    nameInfo.textContent = `Name: ${character.name}`
+
+    const slugInfo = document.querySelector('#slug')
+    slugInfo.textContent = `Slug: ${character.slug}`
+
+    const statsInfo = document.querySelector('#powerstats')
+    const ul = document.querySelector('.powerstats')
+    document.querySelector('.intelligence span').innerText = character.powerstats.intelligence
+    document.querySelector('.strength span').innerText = character.powerstats.strength
+    document.querySelector('.speed span').innerText = character.powerstats.speed
+    document.querySelector('.durability span').innerText = character.powerstats.durability
+    document.querySelector('.power span').innerText = character.powerstats.power
+    document.querySelector('.combat span').innerText = character.powerstats.combat
+    
+    const appearanceInfo = document.querySelector('#appearance')
+    const appearance= document.querySelector('.appearance')
+    document.querySelector('.gender span').innerText = character.appearance.gender
+    document.querySelector('.race span').innerText = character.appearance.race
+    document.querySelector('.height span').innerText = character.appearance.height[0]
+    document.querySelector('.weight span').innerText = character.appearance.weight[0]
+    document.querySelector('.eye_color span').innerText = character.appearance.eyeColor
+    document.querySelector('.hair_color span').innerText = character.appearance.hairColor
+
+    
+    // const biographyInfo = document.querySelector('#biography')
+    // const biography = document.querySelectorAll('.biography')
+    // document.querySelector('.full_name span').innerText = character.biography.fullName
+    // document.querySelector('.alter_ego span').innerText = character.biography.alterEgos
+    // document.querySelector('.aliases span').innerText = character.biography.aliases
+    // document.querySelector('.place_of_birth span').innerText = character.biography.placeOfBirth
+    // document.querySelector('.first_appearance span').innerText = character.biography.firstAppearance
+    // document.querySelector('.publisher span').innerText = character.biography.publisher
+    // document.querySelector('.alignment span').innerText = character.biography.alignment
+   
+
+    const workInfo = document.querySelector('#work')
+    const work =document.querySelector('.work')
+    document.querySelector('.occupation span').innerText = character.work['occupation']
+    document.querySelector('.base span').innerText = character.work.base
+
+    const connectionsInfo = document.querySelector('#connections')
+    const connections = document.querySelector('.connections')
+    document.querySelector('.group_affiliation span').innerText =character.connections['groupAffiliation']
+    document.querySelector('.relatives span').innerText =character.connections['relatives']
+    
+
+    // like operation
+    const likeDiv = document.getElementById('like')
+    
+    if (likeDiv.innerHTML === '') {
+    let likes = 0;
+    let dislikes = 0;
+
+    const thumbsUp = document.createElement('span');
+    thumbsUp.innerHTML = '&#x1F44D;'; 
+    thumbsUp.classList ='thumbs-icon';
+    
+    const thumbsDown = document.createElement('span');
+    thumbsDown.innerHTML = '&#x1F44E;';  
+    thumbsDown.classList = 'thumbs-icon';
+
+    const likesCount = document.createElement('span');
+    likesCount.textContent = `Likes: ${likes}`;
+    const dislikesCount = document.createElement('span');
+    dislikesCount.textContent = `Dislikes: ${dislikes}`;
+
+
+    thumbsUp.addEventListener('click', function() {
+        likes++;
+        likesCount.textContent = `Likes: ${likes}`;
+      });
+      
+      thumbsDown.addEventListener('click', function() {
+        dislikes++;
+        dislikesCount.textContent = `Dislikes: ${dislikes}`;
+      });
+
+    likeDiv.appendChild(thumbsUp);
+    likeDiv.appendChild(thumbsDown);
+    likeDiv.appendChild(likesCount);
+    likeDiv.appendChild(dislikesCount);
+    
 }
-
+}
 
